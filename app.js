ffcac879c9666fb0832c422374f8da3bcdd01955
hamzasid19 <hamasiddiqui19322@gmail.com>
2024-02-12 23:25:55 +0500

    tracking the player turn

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ const App = {
     reset: document.querySelector('[data-id="resetBtn"]'),
     newRound: document.querySelector('[data-id="newBtn"]'),
     squares: document.querySelectorAll('[data-id="square"]'),
+    div: document.querySelector(".action"),
   },
 
   currentSymbol: "fa-o",
   currentColor: "yellow",
+  p1: "Player 1 is up",
+  p2: "Player 2 is up",
 
   init() {
     App.eventListenerFunction();
+    App.updateMove();
   },
 
   eventListenerFunction() {
@@ -28,7 +32,6 @@ const App = {
 
     App.$.squares.forEach((square) => {
       square.addEventListener("click", (event) => {
-        console.log(`the square with id ${square.id} was clicked`);
         if (!square.querySelector(".fa-solid")) {
           const symbol = document.createElement("i");
 
@@ -37,18 +40,37 @@ const App = {
             `${App.currentSymbol}`,
             `${App.currentColor}`
           );
-
           square.appendChild(symbol);
-
           App.currentSymbol = App.currentSymbol === "fa-o" ? "fa-x" : "fa-o";
           App.currentColor =
             App.currentColor === "yellow" ? "turquoise" : "yellow";
-        }
 
-        // <i class="fa-solid fa-o turquoise"></i>
-        // <i class="fa-solid fa-x yellow"></i>
+          App.updateMove();
+        }
       });
     });
+  },
+  updateMove() {
+    while (App.$.div.firstChild) {
+      App.$.div.removeChild(App.$.div.firstChild);
+    }
+    let existingIcon = App.$.div.querySelector(".fa-solid");
+
+    if (existingIcon) {
+      // If the child exists, update its classList
+      existingIcon.classList.remove("fa-o", "fa-x", "yellow", "turquoise");
+      existingIcon.classList.add(App.currentSymbol, App.currentColor);
+    } else {
+      // If no child exists, create a new one and append it
+      const symbol = document.createElement("i");
+      symbol.classList.add("fa-solid", App.currentSymbol, App.currentColor);
+      App.$.div.appendChild(symbol);
+
+      const moveText = document.createElement("p");
+      moveText.textContent = App.currentSymbol === "fa-o" ? App.p1 : App.p2;
+      moveText.classList.add(App.currentColor);
+      App.$.div.appendChild(moveText);
+    }
   },
 };
 
